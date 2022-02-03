0ae95f066f5c06cebac67fe581f34c4df4015ef1
abdfnx <abdfnabd@gmail.com>
2022-02-03 09:13:37 +0300

    create `CtrlKey` function to return the ctrl key of the os

--- constants/constants.go
+++ constants/constants.go
@@ -1,4 +1,17 @@
 package constants
 
+import (
+	"runtime"
+)
+
 const DEFAULT_ADDRESS = "167.71.65.96"
 const DEFAULT_PORT = 80
+
+func CtrlKey() string {
+	// if os is macos, then return "⌘"
+	if runtime.GOOS == "darwin" {
+		return "⌘"
+	} else {
+		return "ctrl"
+	}
+}
