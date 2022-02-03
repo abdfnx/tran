7502fc8a201d06b146452c8a21d7d855b39f8dce
abdfnx <abdfnabd@gmail.com>
2022-02-03 09:37:34 +0300

    add `charmbracelet/bubbles/progress` module, add tran colors and styles

--- constants/constants.go
+++ constants/constants.go
@@ -1,11 +1,20 @@
 package constants
 
 import (
+	"fmt"
 	"time"
 	"runtime"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/bubbles/progress"
 )
+
+const DEFAULT_ADDRESS = "167.71.65.96"
+const DEFAULT_PORT = 80
+
+const SEND_TEMP_FILE_NAME_PREFIX = "tran-send-tmp"
+const RECEIVE_TEMP_FILE_NAME_PREFIX = "tran-receive-tmp"
 
 const (
 	PrimaryBoxActive = iota
@@ -31,12 +40,21 @@ const (
 	MAX_WIDTH               = 80
 	PRIMARY_COLOR           = "#1E90FF"
 	SECONDARY_COLOR         = "#1E6AFF"
+	DARK_GRAY_COLOR         = "#3c3836"
 	START_PERIOD            = 1 * time.Millisecond
 	SHUTDOWN_PERIOD         = 1000 * time.Millisecond
 )
 
-const DEFAULT_ADDRESS = "167.71.65.96"
-const DEFAULT_PORT = 80
+var QuitKeys = []string{"q", "esc"}
+var PadText = strings.Repeat(" ", PADDING)
+var QuitCommandsHelpText = HelpStyle(fmt.Sprintf("(press one of [%s] keys to quit from tran)", (strings.Join(QuitKeys, ", "))))
+var ProgressBar = progress.NewModel(progress.WithGradient(SECONDARY_COLOR, PRIMARY_COLOR))
+
+var baseStyle = lipgloss.NewStyle()
+var InfoStyle = baseStyle.Copy().Foreground(lipgloss.Color(PRIMARY_COLOR)).Render
+var HelpStyle = baseStyle.Copy().Foreground(lipgloss.Color(DARK_GRAY_COLOR)).Render
+var ItalicText = baseStyle.Copy().Italic(true).Render
+var BoldText = baseStyle.Copy().Bold(true).Render
 
 func CtrlKey() string {
 	// if os is macos, then return "⌘"
--- go.mod
+++ go.mod
@@ -2,14 +2,23 @@ module github.com/abdfnx/tran
 
 go 1.17
 
-require github.com/charmbracelet/lipgloss v0.4.0
+require (
+	github.com/charmbracelet/bubbles v0.10.2
+	github.com/charmbracelet/lipgloss v0.4.0
+)
 
 require (
+	github.com/charmbracelet/bubbletea v0.19.3 // indirect
+	github.com/charmbracelet/harmonica v0.1.0 // indirect
+	github.com/containerd/console v1.0.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 )
