87552f4f9f136a722397702252c0cb1d16b033f6
abdfnx <abdfnabd@gmail.com>
2022-02-03 09:27:24 +0300

    add `charmbracelet/lipgloss` module, add new constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,38 @@
 package constants
 
 import (
+	"time"
 	"runtime"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	PrimaryBoxActive = iota
+	SecondaryBoxActive
+	ThirdBoxActive
+)
+
+const (
+	StatusBarHeight      = 1
+	BoxPadding           = 1
+	EllipsisStyle        = "..."
+	FileSizeLoadingStyle = "---"
+)
+
+var BoldTextStyle = lipgloss.NewStyle().Bold(true)
+
+var Colors = map[string]lipgloss.Color{
+	"black": "#000000",
+}
+
+const (
+	PADDING                 = 2
+	MAX_WIDTH               = 80
+	PRIMARY_COLOR           = "#1E90FF"
+	SECONDARY_COLOR         = "#1E6AFF"
+	START_PERIOD            = 1 * time.Millisecond
+	SHUTDOWN_PERIOD         = 1000 * time.Millisecond
 )
 
 const DEFAULT_ADDRESS = "167.71.65.96"
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/abdfnx/tran
 
 go 1.17
+
+require github.com/charmbracelet/lipgloss v0.4.0
+
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
+	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+)
