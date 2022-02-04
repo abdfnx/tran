c407ad0b787a4061a3619d953b37c5d50b85c19c
abdfnx <abdfnabd@gmail.com>
2022-02-04 10:40:42 +0300

    add receiver main struct and functions in `core/receiver` pkg

---
+++ core/receiver/receiver.go
@@ -0,0 +1,51 @@
+package receiver
+
+import (
+	"github.com/abdfnx/tran/models"
+	"github.com/abdfnx/tran/core/crypt"
+)
+
+type Receiver struct {
+	crypt             *crypt.Crypt
+	payloadSize       int64
+	tranxAddress string
+	tranxPort    int
+	ui                chan<- UIUpdate
+	usedRelay         bool
+}
+
+func NewReceiver(programOptions models.TranOptions) *Receiver {
+	return &Receiver{
+		tranxAddress: programOptions.TranxAddress,
+		tranxPort:    programOptions.TranxPort,
+	}
+}
+
+func WithUI(r *Receiver, ui chan<- UIUpdate) *Receiver {
+	r.ui = ui
+	return r
+}
+
+func (r *Receiver) UsedRelay() bool {
+	return r.usedRelay
+}
+
+func (r *Receiver) PayloadSize() int64 {
+	return r.payloadSize
+}
+
+func (r *Receiver) TranxAddress() string {
+	return r.tranxAddress
+}
+
+func (r *Receiver) TranxPort() int {
+	return r.tranxPort
+}
+
+func (r *Receiver) updateUI(progress float32) {
+	if r.ui == nil {
+		return
+	}
+
+	r.ui <- UIUpdate{Progress: progress}
+}
